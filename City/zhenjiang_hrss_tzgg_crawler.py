f3efc59be8a4cb7de08a0638cb5d9afcbe0a0230
njzhx <bsbindex@gmail.com>
2026-08-17 10:02:55 +0800

    新增镇江市各部门栏目爬虫（41个）

---
+++ City/zhenjiang_hrss_tzgg_crawler.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""镇江市人力资源和社会保障局_通知公示 爬虫。
+
+列表页为镇江市政府统一信息公开模板的服务端渲染静态页，
+结构为 li > a[title] + span.time，分页 URL 为 前缀_N.shtml。
+共享抓取逻辑见 zhenjiang_common.py。
+"""
+
+try:
+    from City.zhenjiang_common import run_crawler, scrape_channels
+except ImportError:  # pragma: no cover - 兼容直接运行
+    from zhenjiang_common import run_crawler, scrape_channels
+
+
+TARGET_URL = "http://hrss.zhenjiang.gov.cn/hrss/tzgg/list.shtml"
+SOURCE_NAME = "镇江市人力资源和社会保障局_通知公示"
+CATEGORY = "镇江"
+
+CHANNEL_URLS = [
+    "http://hrss.zhenjiang.gov.cn/hrss/tzgg/list.shtml",
+]
+
+
+def scrape_data():
+    """返回 (policies, latest_items, metrics)。"""
+    return scrape_channels(SOURCE_NAME, CHANNEL_URLS, CATEGORY)
+
+
+def run():
+    """执行抓取、统一保存，并返回 CrawlerRunResult。"""
+    return run_crawler(SOURCE_NAME, CHANNEL_URLS, CATEGORY)
+
+
+if __name__ == "__main__":
+    run()
